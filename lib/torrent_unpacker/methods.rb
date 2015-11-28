9353add8d8909066909102d1146d0b806100f40b
yaroslav zemlyanuhin <y.zemlyanuhin@cti.ru>
2015-11-28 20:39:16 +0300

    Added error handlers for incorrect dir and incorrect start item

--- lib/torrent_unpacker/methods.rb
+++ lib/torrent_unpacker/methods.rb
@@ -1,10 +1,21 @@
 def extract_movie(source, dest, start_item)
-  sorted = Dir[source + '*'].sort do |a, b|
+  dirs_list = Dir[source + '*']
+  if dirs_list.empty?
+    puts "Incorrect source dir #{source}\n"
+    return []
+  end
+
+  sorted = dirs_list.sort do |a, b|
     a[/S\d{2}E\d{2}/] <=> b[/S\d{2}E\d{2}/]
   end
 
   last_index = sorted.index do |item|
     item =~ /#{start_item}/
+  end
+
+  if last_index.nil?
+    puts "Incorrect start item #{start_item} in #{source}\n"
+    return []
   end
 
   cutted = sorted.slice(last_index + 1, sorted.length)
@@ -14,7 +25,7 @@ def extract_movie(source, dest, start_item)
 
     if archive
       print "Unpacking '#{archive}' to '#{dest}'\n"
-      system "unrar e -y #{archive} '#{dest}' > /dev/null"
+      system "unrar e -y #{archive} '#{dest}'"
       archive[/S\d{2}E\d{2}/]
     end
   end
